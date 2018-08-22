3d20281ba829f4dd5e20dbc0828b44629149a06e
RemonJia <jia_ymeng@163.com>
2018-08-22 15:00:27 +0800

    测试提交git

---
+++ SpringCloudRegistry/src/main/java/com/jym/microservice/MicroserviceController.java
@@ -0,0 +1,17 @@
+package com.jym.microservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/microservice")
+public class MicroserviceController {
+	@RequestMapping("/register")
+	public String hello(){
+		return "注册成功";
+	}
+	@RequestMapping("/login")
+	public String haha(){
+		return "欢迎大佬";
+	}
+}

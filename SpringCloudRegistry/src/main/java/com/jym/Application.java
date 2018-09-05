f8c3e464975d02120fea564d2ee19b04932d50cc
RemonJia <jia_ymeng@163.com>
2018-09-05 17:40:28 +0800

    dsf

--- SpringCloudRegistry/src/main/java/com/jym/Application.java
+++ SpringCloudRegistry/src/main/java/com/jym/Application.java
@@ -1,9 +1,13 @@
 package com.jym;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@MapperScan("com.jym")
 @EnableEurekaServer
 @SpringBootApplication
 public class Application {
--- SpringCloudRegistry/src/main/java/com/jym/microservice/MicroserviceController.java
+++ SpringCloudRegistry/src/main/java/com/jym/microservice/MicroserviceController.java
@@ -1,18 +1,28 @@
 package com.jym.microservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jym.bean.DatabaseInfo;
+import com.jym.dao.DatabaseInfoDao;
 
 @RestController
 @RequestMapping("/microservice")
 public class MicroserviceController {
+	
+	@Autowired
+	private DatabaseInfoDao diDao;
+	  
 	@RequestMapping("/register")
 	public String hello(){
 		//你还哦
 		return "注册成功";
 	}
 	@RequestMapping("/login")
-	public String haha(){
-		return "欢迎";
+	public DatabaseInfo haha(){
+		DatabaseInfo di = diDao.getDatabaseInfoById(1);
+		System.out.println(di.getUsername());		
+		return di;
 	}
 }

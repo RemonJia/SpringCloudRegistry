faea8f379ff36277b625e2e285c8a2fd55028671
RemonJia <jia_ymeng@163.com>
2018-09-06 15:02:53 +0800

    增加页面及mybatis

--- SpringCloudRegistry/src/main/java/com/jym/microservice/MicroserviceController.java
+++ SpringCloudRegistry/src/main/java/com/jym/microservice/MicroserviceController.java
@@ -3,17 +3,23 @@ package com.jym.microservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.jym.bean.DatabaseInfo;
 import com.jym.dao.DatabaseInfoDao;
 
 @RestController
-@RequestMapping("/microservice")
+@RequestMapping("/ms")
 public class MicroserviceController {
 	
 	@Autowired
-	private DatabaseInfoDao diDao;
-	  
+	private DatabaseInfoDao diDao;	
+
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}  
+	
 	@RequestMapping("/register")
 	public String hello(){
 		//你还哦
